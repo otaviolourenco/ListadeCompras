e894d7bc59e9727fc64ab63f5a7b0d99a44c3017
Otávio Lourenço <otavioluissv@gmail.com>
2023-03-28 19:25:53 +0100

    Initial commit

---
+++ addDadosLista.js
@@ -0,0 +1,99 @@
+// Seleciona os campos de entrada de preço unitário e quantidade
+var unitPriceField = document
+  .getElementById("uniPrice")
+  .addEventListener("input", function () {
+    let value = this.value;
+  });
+var quantityField = document.getElementById("quanti");
+
+// Adiciona um ouvinte de evento de mudança de valor para cada campo
+unitPriceField.addEventListener("input", updateTotalPrice);
+quantityField.addEventListener("input", updateTotalPrice);
+
+// Função para atualizar o preço total com base no preço unitário e na quantidade
+function updateTotalPrice() {
+  // Seleciona o campo de entrada de preço total
+  var totalPriceField = document.getElementById("totalPrice");
+
+  // Obtém os valores atuais do preço unitário e da quantidade
+  var unitPrice = unitPriceField.value;
+  var quantity = quantityField.value;
+
+  // Calcula o preço total multiplicando o preço unitário pela quantidade
+  var totalPrice = unitPrice * quantity;
+
+  // Define o valor do campo de entrada de preço total como o novo preço total calculado
+  totalPriceField.value = totalPrice;
+}
+
+// Seleciona o botão "Adicionar item"
+var addItemBtn = document.getElementById("addItem");
+
+// Adiciona um ouvinte de evento de clique ao botão
+addItemBtn.addEventListener("click", function () {
+  // Seleciona os valores digitados nos campos de entrada
+  var productName = document.getElementById("ProduName").value;
+  var unitPrice = unitPriceField.value;
+  var quantity = quantityField.value;
+  var totalPrice = unitPrice * quantity;
+
+  // Cria uma nova linha para a tabela
+  var newRow = document.createElement("tr");
+
+  // Adiciona células à nova linha com os valores inseridos pelo usuário
+  newRow.innerHTML =
+    "<td>" +
+    productName +
+    "</td><td>" +
+    unitPrice +
+    "</td><td>" +
+    quantity +
+    "</td><td>" +
+    totalPrice +
+    "</td><td><button class='deleteBtn'>Excluir</button></td><td><button class='editBtn'>Editar</button></td>";
+
+  //Exibir mensagem de sucesso ao add item
+  function showSuccessMessage() {
+    document.getElementById("successMessage").style.display = "block";
+    setTimeout(function showSuccessMessage() {
+      document.getElementById("successMessage").style.display = "none";
+    }, 2000);
+  }
+  showSuccessMessage();
+
+  // Seleciona a tabela de itens
+  var itemList = document.getElementById("listHeader");
+
+  // Adiciona a nova linha à tabela de itens
+  itemList.appendChild(newRow);
+
+  // Seleciona os botões "Excluir" e "Editar" para a nova linha
+  var deleteBtn = newRow.querySelector(".deleteBtn");
+  var editBtn = newRow.querySelector(".editBtn");
+
+  // Adiciona um ouvinte de evento de clique para o botão "Excluir"
+  deleteBtn.addEventListener("click", function () {
+    // Remove a linha correspondente da tabela
+    newRow.remove();
+  });
+
+  // Adiciona um ouvinte de evento de clique para o botão "Editar"
+  editBtn.addEventListener("click", function () {
+    // Define os valores dos campos de entrada como os valores da linha correspondente
+    document.getElementById("ProduName").value = productName;
+    unitPriceField.value = unitPrice;
+    quantityField.value = quantity;
+
+    // Calcula o preço total com base nos novos valores de preço unitário e quantidade
+    updateTotalPrice();
+
+    // Remove a linha correspondente da tabela
+    newRow.remove();
+  });
+
+  // Limpa os valores dos campos de entrada
+  document.getElementById("ProduName").value = "";
+  unitPriceField.value = "";
+  quantityField.value = "";
+  document.getElementById("totalPrice").value = "";
+});
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Lista de compras</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="container" style="text-align: center;">
+            <h1>Lista de Compras</h1>
+        </div>
+    </header>
+
+    <div class="container" style="padding: 0 50px;">
+        <div class="col-sm-6 addProduct">
+            <form id="myList" style="flex-direction: row; justify-content: center; text-align: center; flex-wrap: nowrap;">
+                <span id="successMessage">Item adicionado! </span>
+                <input type="text" id="ProduName" class="inputProduct" placeholder="Nome do produto" required>
+
+                <input type="number" step="0.10" id="uniPrice" class="inputProduct" placeholder="Preço Unid." required>
+                <input type="number" step="0.01" id="quanti" class="inputProduct" placeholder="Quantidade" min="1" required>
+                <input type="number" id="totalPrice" class="inputProduct" placeholder="Preço Total." disabled>
+            </form><br>
+            <button id="addItem">Adicionar item</button>
+        </div>
+    </div>
+
+    <div id="list" style="padding: 20px;">
+        <div class="col-sm-6" style="display: flex; justify-content: center; padding-top: 20px;">
+            <table id="listHeader" style="text-align: center;">
+                <tr>
+                    <th>Produto</th>
+                    <th>Preço</th>
+                    <th>Quantidade</th>
+                    <th>Preço total</th>
+                </tr>
+                <tr style="text-align: center;">
+                    <td> • </td>
+                    <td> • </td>
+                    <td> • </td>
+                    <td> • </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+
+    <script src="addDadosLista.js"></script>
+    <script src="precosForm.js"></script>
+</body>
+
+</html>
